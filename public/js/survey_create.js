abfd8787bd4cc6562a19885ed375a26e14594a8e
Kevin Solorio <soloriok@gmail.com>
2013-07-30 17:46:47 -0500

    created variables for dom access
    makes the look up on these elements faster.

--- public/js/survey_create.js
+++ public/js/survey_create.js
@@ -1,48 +1,54 @@
 
 $(document).ready(function(){
-  $('#new-question').hide();
-  $('#create').hide();
-  $('#add-option').hide();
-  $('#options').hide();
-  $('#responses').hide();
+  var $newQuestion = $('#new-question'),
+      $create      = $('#create'),
+      $addOption   = $('#add-option'),
+      $options     = $('#options'),
+      $responses   = $('#responses');
+
+  $newQuestion.hide();
+  $create.hide();
+  $addOption.hide();
+  $options.hide();
+  $responses.hide();
 
   $('#add').on('click', function(event){
     event.preventDefault();
     event.stopPropagation();
     $('#add-question').hide();
-    $('#new-question').show();
+    $newQuestion.show();
 
     $('#type').on('change', function(event){
       event.stopPropagation();
-      $('#create').show();
+      $create.show();
       var type = $(this).val();
 
       if (type == 'TextArea' || type == 'NumSlider' || type == 'PercentSlider' || type == 'ShortResponse') {
-        $('#add-option').hide();
+        $addOption.hide();
         $('#options p').remove();
-        $('#options').hide();
-        $('#responses').hide();
+        $options.hide();
+        $responses.hide();
       }
 
       if (type == 'McRadio' || type == 'McCheck') {
-        $('#add-option').show();
-        $('#options').show();
-        $('#responses').hide();
+        $addOption.show();
+        $options.show();
+        $responses.hide();
 
-        $('#add-option').off();
-        $('#add-option').on('click', function(event){
+        $addOption.off();
+        $addOption.on('click', function(event){
           event.stopPropagation();
-          $('#options').append($('<p id="option"><input type="text" name="response" placeholder="Option"><button id="remove">Remove</button></p>'));
+          $options.append($('<p id="option"><input type="text" name="response" placeholder="Option"><button id="remove">Remove</button></p>'));
         });
-        $('#options').on('click', '#remove', function(event){
+        $options.on('click', '#remove', function(event){
           event.stopPropagation();
           $(this).parent().remove();
         });
       }
     });
 
-    $('#create').off();
-    $('#create').on('click', function(event){
+    $create.off();
+    $create.on('click', function(event){
       event.stopPropagation();
       var url = $('#define').attr('action');
       var responses = [];
@@ -108,7 +114,7 @@ $(document).ready(function(){
 function resetForm(){
   $('#text').val('');
   $('#type').val('');
-  $('#new-question').hide();
+  $newQuestion.hide();
   $('#add-question').show();
   $('#create').hide();
   $('#add-option').hide();
